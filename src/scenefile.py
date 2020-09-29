820e510b4b77b6753d39f85d1f1592cb75af441c
beardsleyAmory <43458650+beardsleyAmory@users.noreply.github.com>
2020-09-29 18:48:58 -0500

    Assignment 3 - commit 2
    Caught up with video 06 -  file saves and creates new directories if necessary

--- src/scenefile.py
+++ src/scenefile.py
@@ -1,4 +1,9 @@
+import logging
+
+import pymel.core as pmc
 from pymel.core.system import Path
+
+log = logging.getLogger(__name__)
 
 class SceneFile(object):
     """An abstract representation of a Scene file."""
@@ -30,12 +35,15 @@ class SceneFile(object):
         self.descriptor, self.task, ver = path.name.stripext().split("_")
         self.ver = int(ver.split("v")[-1])
 
+    def save(self):
+        """save the scene file
 
-scene_file = SceneFile("D:/tank_model_v001.ma")
-print(scene_file.path)
-print(scene_file.filename)
-print(scene_file.folder_path)
-print(scene_file.descriptor)
-print(scene_file.task)
-print(scene_file.ver)
-print(scene_file.ext)
+        Returns
+            Path: The path to the scene file if successful
+        """
+        try:
+            return pmc.system.saveAs(self.path)
+        except RuntimeError as err:
+            log.warning("Missing directories in path. Creating folders...")
+            self.folder_path.makedirs_p()
+            return pmc.system.saveAs(self.path)
